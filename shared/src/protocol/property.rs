120baf927bf23367242c6b41db376214dd91123e
Charles Bournhonesque <cbournhonesque@snapchat.com>
2022-12-16 14:30:29 -0500

    f

--- shared/src/protocol/property.rs
+++ shared/src/protocol/property.rs
@@ -32,7 +32,7 @@ impl<T: Serde> AddAssign for Property<T>
     where T: AddAssign {
 
     fn add_assign(&mut self, rhs: Self) {
-        *self += rhs.inner
+        self.deref_mut() += rhs.inner;
     }
 }
 
@@ -51,7 +51,7 @@ impl<T: Serde> MulAssign<f32> for Property<T>
     where T: MulAssign<f32> {
 
     fn mul_assign(&mut self, rhs: f32) {
-        *self *= rhs
+        self.deref_mut() *= rhs;
     }
 }
 
