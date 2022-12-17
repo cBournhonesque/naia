4ccf2f7ac88d3c74d9abe443f570ac9c6515389b
Charles Bournhonesque <cbournhonesque@snapchat.com>
2022-12-16 19:15:02 -0500

    fix lifetimes

--- shared/src/protocol/property.rs
+++ shared/src/protocol/property.rs
@@ -25,7 +25,7 @@ impl<T: Serde> Serialize for Property<T> where T: Serialize {
 
 /// Deserialize the property according to the underlying field's deserializer
 /// Again, same issues as with Default, we had to use 0 as mutator index
-impl<'de, T: Serde> Deserialize<'de> for Property<T> where T: Deserialize {
+impl<'de, T: Serde> Deserialize<'de> for Property<T> where T: Deserialize<'de> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
         let inner = T::deserialize(deserializer)?;
         Ok(Self::new(inner, 0))
